bb40594d168da7b71e5a486d668265c01efae428
pellax <tuxisalive@gmail.com>
2021-08-26 11:04:09 +0200

    setup anyadido

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 #include "loramesher.h"
 
+LoraMesher* radio ;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  radio = new LoraMesher();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
 }
