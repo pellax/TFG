7056147ee5cd3d03bb223f7df12bc354022116ea
pellax <tuxisalive@gmail.com>
2022-08-30 19:07:56 +0200

    Main updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,35 @@
 #include <Arduino.h>
 #include "LoraMesher.h"
+#include "display.h"
 #include "network.h"
 
-#define BOARD_LED 4
+//Using LILYGO TTGO T-BEAM v1.1 
+#define BOARD_LED   4
+#define LED_ON      LOW
+#define LED_OFF     HIGH
 
-LoraMesher& radio = LoraMesher::getInstance() ;
-uint32_t dataCounter2 = 0;
-uint32_t dataCounter1 = 0;
-uint32_t dataCounter3 = 0;
-uint32_t dataCounter4 = 0;
+LoraMesher& radio = LoraMesher::getInstance();
+
 uint32_t dataCounter = 0;
 struct dataPacket {
-    uint32_t counter = 0;
+    uint32_t counter[35] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34};
 };
 
 dataPacket* helloPacket = new dataPacket;
 
-//Led flash
+/**
+ * @brief Flash the lead
+ *
+ * @param flashes number of flashes
+ * @param delaymS delay between is on and off of the LED
+ */
 void led_Flash(uint16_t flashes, uint16_t delaymS) {
     uint16_t index;
     for (index = 1; index <= flashes; index++) {
-        digitalWrite(BOARD_LED, HIGH);
-        delay(delaymS);
-        digitalWrite(BOARD_LED, LOW);
-        delay(delaymS);
+        digitalWrite(BOARD_LED, LED_ON);
+        vTaskDelay(delaymS / portTICK_PERIOD_MS);
+        digitalWrite(BOARD_LED, LED_OFF);
+        vTaskDelay(delaymS / portTICK_PERIOD_MS);
     }
 }
 
@@ -32,8 +38,12 @@ void led_Flash(uint16_t flashes, uint16_t delaymS) {
  *
  * @param data
  */
-void printPacket(dataPacket data) {
-    Log.verboseln(F("Hello Counter received nº %X" CR), data.counter);
+void printPacket(dataPacket* data, uint16_t sourceAddress) {
+    char text[32];
+    snprintf(text, 32, ("%X-> %d"), sourceAddress, data->counter[0]);
+
+    Screen.changeLineThree(String(text));
+    Log.verboseln(F("Received data nº %d"), data->counter[0]);
 }
 
 /**
@@ -42,16 +52,31 @@ void printPacket(dataPacket data) {
  * @param packet
  */
 void printDataPacket(AppPacket<dataPacket>* packet) {
-    Log.traceln(F("Packet arrived from %X with size %d" CR), packet->src, packet->payloadSize);
+    Log.traceln(F("Packet arrived from %X with size %d bytes"), packet->src, packet->payloadSize);
 
     //Get the payload to iterate through it
     dataPacket* dPacket = packet->payload;
     size_t payloadLength = packet->getPayloadLength();
 
+    printPacket(&dPacket[0], packet->src);
+
+    Log.traceln(F("---- Payload ---- Payload length in dataP: %d "), payloadLength);
+    Log.setShowLevel(false);
+
     for (size_t i = 0; i < payloadLength; i++) {
-        //Print the packet
-        printPacket(dPacket[i]);
+        Log.verbose(F("Received data nº %d"), i);
+        Log.verbose(F("%d -- "), i);
+
+        for (size_t j = 0; j < 35; j++) {
+            Log.verbose(F("%d, "), dPacket[i].counter[j]);
+
+        }
+        Log.verbose(F(""));
     }
+
+    Log.setShowLevel(true);
+    Log.traceln(F("---- Payload Done ---- "));
+
 }
 
 /**
@@ -66,8 +91,8 @@ void processReceivedPackets(void*) {
 
         //Iterate through all the packets inside the Received User Packets FiFo
         while (radio.getReceivedQueueSize() > 0) {
-            Log.traceln(F("ReceivedUserData_TaskHandle notify received" CR));
-            Log.traceln(F("Fifo receiveUserData size: %d" CR), radio.getReceivedQueueSize() > 0);
+            Log.traceln(F("ReceivedUserData_TaskHandle notify received"));
+            Log.traceln(F("Fifo receiveUserData size: %d"), radio.getReceivedQueueSize() > 0);
 
             //Get the first element inside the Received User Packets FiFo
             AppPacket<dataPacket>* packet = radio.getNextAppPacket<dataPacket>();
@@ -77,8 +102,30 @@ void processReceivedPackets(void*) {
 
             //Delete the packet when used. It is very important to call this function to release the memory of the packet.
             radio.deletePacket(packet);
+
         }
     }
+}
+
+TaskHandle_t receiveLoRaMessage_Handle = NULL;
+
+/**
+ * @brief Create a Receive Messages Task and add it to the LoRaMesher
+ *
+ */
+void createReceiveMessages() {
+    int res = xTaskCreate(
+        processReceivedPackets,
+        "Receive App Task",
+        4096,
+        (void*) 1,
+        2,
+        &receiveLoRaMessage_Handle);
+    if (res != pdPASS) {
+        Log.errorln(F("Receive App Task creation gave error: %d"), res);
+    }
+
+    radio.setReceiveAppDataTaskHandle(receiveLoRaMessage_Handle);
 }
 
 /**
@@ -87,9 +134,113 @@ void processReceivedPackets(void*) {
  */
 void setupLoraMesher() {
     //Init the loramesher with a processReceivedPackets function
-    radio.init(processReceivedPackets);
+    radio.begin();
 
-    Serial.println("Lora initialized");
+    //Create the receive task and add it to the LoRaMesher
+    createReceiveMessages();
+
+    //Start LoRaMesher
+    radio.start();
+
+    Log.verboseln("Lora initialized");
+}
+
+/**
+ * @brief Displays the address in the first line
+ *
+ */
+void printAddressDisplay() {
+    char addrStr[15];
+    snprintf(addrStr, 15, "Id: %X\r\n", radio.getLocalAddress());
+
+    Screen.changeLineOne(String(addrStr));
+}
+
+/**
+ * @brief Print the routing table into the display
+ *
+ */
+void printRoutingTableToDisplay() {
+
+    //Set the routing table list that is being used and cannot be accessed (Remember to release use after usage)
+    LM_LinkedList<RouteNode>* routingTableList = radio.routingTableList();
+
+    routingTableList->setInUse();
+
+    Screen.changeSizeRouting(radio.routingTableSize());
+
+    char text[15];
+    for (int i = 0; i < radio.routingTableSize(); i++) {
+        RouteNode* rNode = (*routingTableList)[i];
+        NetworkNode node = rNode->networkNode;
+        snprintf(text, 15, ("|%X(%d)->%X"), node.address, node.metric, rNode->via);
+        Screen.changeRoutingText(text, i);
+    }
+
+    //Release routing table list usage.
+    routingTableList->releaseInUse();
+
+    Screen.changeLineFour();
+}
+
+/**
+ * @brief Every 20 seconds it will send a counter to a position of the dataTable
+ *
+ */
+void sendLoRaMessage(void*) {
+    int dataTablePosition = 0;
+
+    for (;;) {
+        if (radio.routingTableSize() == 0) {
+            vTaskDelay(120000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        if (radio.routingTableSize() <= dataTablePosition)
+            dataTablePosition = 0;
+
+        LM_LinkedList<RouteNode>* routingTableList = radio.routingTableList();
+
+        uint16_t addr = (*routingTableList)[dataTablePosition]->networkNode.address;
+
+        Log.traceln(F("Send data packet nº %d to %X (%d)"), dataCounter, addr, dataTablePosition);
+
+        dataTablePosition++;
+
+        //Create packet and send it.
+        radio.createPacketAndSend(addr, helloPacket, 1);
+
+        //Print second line in the screen
+        Screen.changeLineTwo("Send " + String(dataCounter));
+
+        //Increment data counter
+        helloPacket->counter[0] = dataCounter++;
+
+        //Print routing Table to Display
+        printRoutingTableToDisplay();
+
+        //Wait 20 seconds to send the next packet
+        vTaskDelay(120000 / portTICK_PERIOD_MS);
+    }
+}
+
+/**
+ * @brief Setup the Task to create and send periodical messages
+ *
+ */
+void createSendMessages() {
+    TaskHandle_t sendLoRaMessage_Handle = NULL;
+    BaseType_t res = xTaskCreate(
+        sendLoRaMessage,
+        "Send LoRa Message routine",
+        4098,
+        (void*) 1,
+        1,
+        &sendLoRaMessage_Handle);
+    if (res != pdPASS) {
+        Log.errorln(F("Send LoRa Message task creation gave error: %d"), res);
+        vTaskDelete(sendLoRaMessage_Handle);
+    }
 }
 
 void setupMonitoring() {
@@ -98,69 +249,21 @@ void setupMonitoring() {
     Serial.println("Network initialized");
 }
 
-
-
-
-
-    
-
-
-
 void setup() {
     Serial.begin(115200);
-
-    Serial.println("initBoard");
     pinMode(BOARD_LED, OUTPUT); //setup pin as output for indicator LED
+
+    Screen.initDisplay();
+    Log.verboseln("Board Init");
+
+
     led_Flash(2, 125);          //two quick LED flashes to indicate program start
     setupLoraMesher();
     setupMonitoring();
-
+    printAddressDisplay();
+    createSendMessages();
 }
 
-
 void loop() {
-
-
-/* 
-    Log.trace(F("Send packet %d" CR), dataCounter);
-    helloPacket->counter = dataCounter++;
-
-    //Create packet and send it.
-    //radio->createPacketAndSend(BROADCAST_ADDR, helloPacket, 1);
-
-    //Wait 30 seconds to send the next packet
-    vTaskDelay(30000 / portTICK_PERIOD_MS);
-
-
-  */
-    if (radio.getLocalAddress() == 0xC4F0&& dataCounter1 <=60) {
-        Log.trace(F("Send packet %d" CR), dataCounter1);
-        helloPacket->counter = dataCounter1++;
-
-        //Create packet and send it.
-        radio.createPacketAndSend(0xD39C, helloPacket, 1);
-    }
-    if(radio.getLocalAddress() == 0xD39C&& dataCounter2 <= 60){
-        Log.trace(F("Send packet %d"CR),dataCounter2);
-        helloPacket->counter = dataCounter2++;
-        radio.createPacketAndSend(0x3A0C,helloPacket,1);
-    }
-    if(radio.getLocalAddress() == 0x3A0C&& dataCounter3 <= 60){
-        Log.trace(F("Send packet %d"CR),dataCounter3);
-        helloPacket->counter = dataCounter3++;
-        radio.createPacketAndSend(0x4E64,helloPacket,1);
-        }
-
-        if(radio.getLocalAddress() == 0x4E64 && dataCounter4 <= 60){
-        Log.trace(F("Send packet %d"CR),dataCounter4);
-        helloPacket->counter = dataCounter4++;
-        radio.createPacketAndSend(0xC4F0,helloPacket,1);
-    }
-
-    //Wait 30 seconds to send the next packet
-    vTaskDelay(30000 / portTICK_PERIOD_MS);
-    Log.trace(F("Another while loop"CR));
-
-
-
+    Screen.drawDisplay();
 }
